b706baf75001b4f618e2d7d4ff86c98d66e5a3bb
Lorenz <lf.equihua@gmail.com>
2023-02-20 10:32:02 -0600

    Avances 20 feb

--- Scripting_2023_T1/Scripting_2023_T1.cpp
+++ Scripting_2023_T1/Scripting_2023_T1.cpp
@@ -3,9 +3,48 @@
 #include "SFML/System.hpp"
 #include "SFML/Window.hpp"
 #include "GameObject.h"
+#include <string>
+
+extern "C" 
+{
+#include "Lua542/include/lua.h"
+#include "Lua542/include/lauxlib.h"
+#include "Lua542/include/lualib.h"
+}
+
+#ifdef _WIN32
+#pragma comment(lib, "lua542/liblua54.a")
+#endif // _WIN32
+
+using namespace std;
 
 int main()
 {
+    string codeLine = "x = 4 + 2";
+
+    lua_State* L = luaL_newstate();
+
+    int resultCode = luaL_dostring(L, codeLine.c_str());
+
+    if (resultCode == LUA_OK) 
+    {
+        lua_getglobal(L, "x");
+        if (lua_isnumber(L, -1))
+        {
+            int x_in_cpp = lua_tonumber(L, -1);
+            cout << x_in_cpp << " \n";
+        }
+    }
+
+    else
+    {
+        string errorMessage = lua_tostring(L, -1);
+
+        cout << errorMessage << "\n";
+    }
+
+    lua_close(L);
+
     sf::RenderWindow window(sf::VideoMode(540, 540), "Scripting Game", sf::Style::Default);
     sf::Event m_event;
     GameObject gameObject;
